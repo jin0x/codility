b40549e277012451cf0a123d0a119ca9561e4496
John Leskas <john.leskas@gmail.com>
2022-01-18 15:21:38 +0200

    :sparkles: Initial counter component implementation

---
+++ src/components/counter/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Button } from "./styles";
+
+function Counter({ initialCount, step }) {
+  const [counter, setCounter] = React.useState(initialCount);
+
+  const increment = () => setCounter(counter + step);
+  const decrement = () => setCounter(counter - step);
+
+  return (
+    <div>
+      <p>{counter}</p>
+      <Button onClick={decrement}>Decrement</Button>
+      <Button onClick={increment}>Increment</Button>
+    </div>
+  );
+}
+
+Counter.propTypes = {
+  initialCount: PropTypes.number.isRequired,
+  step: PropTypes.number.isRequired,
+};
+
+Counter.defaultProps = {
+  initialCount: 42,
+  step: 1,
+};
+
+export default Counter;
