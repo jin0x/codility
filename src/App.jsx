6a43af7105050841aac37418cd04492aac33eae9
John Leskas <john.leskas@gmail.com>
2022-01-18 16:19:25 +0200

    :sparkles: Add Todo and Autocomplete exercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import Counter from "./components/Counter";
+import Todo from "./components/Todo";
+import Autocomplete from "./components/autocomplete";
 import HeadingWrapper from "./primitives/heading";
 
 import "./index.scss";
@@ -9,6 +11,12 @@ const App = () => (
   <div className="max-w-6xl mx-auto mt-10 text-3xl">
     <HeadingWrapper>Counter Exercise</HeadingWrapper>
     <Counter />
+    <hr />
+    <HeadingWrapper>Todo Exercise</HeadingWrapper>
+    <Todo />
+    <hr />
+    <HeadingWrapper>Autocomplete Exercise</HeadingWrapper>
+    <Autocomplete />
     <hr />
   </div>
 );
