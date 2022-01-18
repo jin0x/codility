d6f49321011459e44e3bf69e53422e51e22790dd
John Leskas <john.leskas@gmail.com>
2022-01-18 17:25:07 +0200

    :lipstick: Styling improvements

--- src/components/autocomplete/styles.js
+++ src/components/autocomplete/styles.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import ClipLoader from "react-spinners/ClipLoader";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  position: relative;
+`;
 
 export const InputWrapper = styled.div`
   border: 1px solid ${(props) => (props.loading ? "#111" : "#ccc")};
@@ -11,14 +13,22 @@ export const InputWrapper = styled.div`
 
 export const SpinnerWrapper = styled.div`
   position: absolute;
-  right: 0;
-  top: 0;
+  right: 0.25rem;
+  top: 0.5rem;
 `;
 
-export const List = styled.ul``;
+export const List = styled.ul`
+  background-color: #eee;
+  width: 240px;
+`;
 
 export const ListItem = styled.li`
   list-style: none;
+  font-size: 1rem;
+  padding: 0.25rem 0.5rem;
 `;
 
-export const Error = styled.p``;
+export const Error = styled.p`
+  color: red;
+  font-size: 1rem;
+`;
